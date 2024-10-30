8efdbd86ac8d276437fb734991c38391e8fbb8f7
duong.ph2 <duong.ph2@shbfinance.com.vn>
2024-10-30 17:26:09 +0700

    adding header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { AllRoutes } from "./routes/AllRoutes";
 
 import './App.css';
+import {Footer, Header} from "./components";
 
 function App() {
     return (
         <div>
+            <Header />
             <AllRoutes />
+            <Footer />
         </div>
     );
 }
